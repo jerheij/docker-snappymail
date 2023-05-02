f77aff128de86d140202934f5b7e1b7334ab9331
jerheij <14292978+jerheij@users.noreply.github.com>
2023-05-02 17:35:27 +0100

    Update run.sh

--- rootfs/usr/local/bin/run.sh
+++ rootfs/usr/local/bin/run.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # Set configurations
-sed -i "s/<UPLOAD_MAX_SIZE>/$UPLOAD_MAX_SIZE/g" /etc/php.ini
+sed -i "s/<UPLOAD_MAX_SIZE>/$UPLOAD_MAX_SIZE/g" /php.ini
 sed -i "s/<UPLOAD_MAX_SIZE>/$UPLOAD_MAX_SIZE/g" /etc/nginx/sites-enabled/snappymail.conf
 sed -i "s/<SERVER_NAME>/$SERVER_NAME/g" /etc/nginx/sites-enabled/snappymail.conf
-sed -i "s/<MEMORY_LIMIT>/$MEMORY_LIMIT/g" /etc/php/7.4/fpm/php.ini
+sed -i "s/<MEMORY_LIMIT>/$MEMORY_LIMIT/g" /php.ini
 
 # Copy php.ini
-# cat /php.ini > /etc/php/7.4/fpm/php.ini
+cat /php.ini > /etc/php/7.4/fpm/php.ini
 
 # Add folders
 # mkdir -p /rainloop/data/_data_/_default_/plugins/
